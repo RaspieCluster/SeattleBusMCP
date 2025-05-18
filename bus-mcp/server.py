b6c968f7bb3837f5a3f6e64f310a1ce7960d5293
John Koomson <55421895+John-K408@users.noreply.github.com>
2025-05-17 21:52:34 -0700

    add one bus server tool to get current timestamp

--- bus-mcp/server.py
+++ bus-mcp/server.py
@@ -1,7 +1,14 @@
+from typing import Any, Dict
 from mcp.server.fastmcp import FastMCP
 import asyncio
+import requests
 
-mcp = FastMCP("Azure Data Explorer MCP")
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+one_bus_away_api_key = os.getenv('ONE_BUS_AWAY_API_KEY')
+mcp = FastMCP("One Bus Away MCP Server")
 
 @mcp.tool(description="MCP Tool to print hello world")
 async def print_hello(input_name: str) -> str:
@@ -9,8 +16,18 @@ async def print_hello(input_name: str) -> str:
     print("Hello, world!")
     return f"Hello {input_name}!"
 
+@mcp.tool(description="MCP Tool to get the current time")
+async def get_current_time() -> Dict[str, Any]:
+    request_path = f"https://api.pugetsound.onebusaway.org/api/where/current-time.json?key={one_bus_away_api_key}"
+    response = requests.get(request_path)
+    result = response.json()
+    print(f"{result}\n")
+    return result
+    
+
 # test bed
 if __name__ == "__main__":
     print("I am in here")
     asyncio.run(print_hello("This is a test"))
+    asyncio.run(get_current_time())
     mcp.run()
