4bb5d2bfe35b12c994f7883522c2a8e70bcc5572
Mubbie Idoko <66747577+mubbie@users.noreply.github.com>
2025-05-17 19:48:16 -0700

    Add python doc string

--- bus-mcp/server.py
+++ bus-mcp/server.py
@@ -5,6 +5,14 @@ mcp = FastMCP("Azure Data Explorer MCP")
 
 @mcp.tool(description="MCP Tool to print hello world")
 async def print_hello(input_name: str) -> str:
+    """Small MCP Tool
+
+    Args:
+        input_name (str): String to print out
+
+    Returns:
+        str: Greeting message
+    """
     print(f"Hello {input_name}!")
     print("Hello, world!")
     return f"Hello {input_name}!"
