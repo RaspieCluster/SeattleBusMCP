fdb87d63cd721d1cab7d56c30827c0502cd7c0a1
John Koomson <55421895+John-K408@users.noreply.github.com>
2025-07-12 15:58:13 -0700

    nit

--- bus-mcp/server.py
+++ bus-mcp/server.py
@@ -221,10 +221,11 @@ async def get_current_time() -> Dict[str, Any]:
     return result
 
 @mcp.tool(description="MCP Tool to get the next bus stops from a provided bus stop_id in the Puget Sound, Washington Area")
-async def get_next_stop(stop_id) ->set:
-    request_path = f"{ONE_BUS_AWAY_BASE_URL}/{ARRIVALS_AND_DEPARTURES_API}?key={one_bus_away_api_key}".replace("{stop_id}",stop_id)
-    response = requests.get(request_path)
-    result = response.json()
+async def get_next_stop(stop_id, minutes_ahead = 60) ->set:
+    params = {
+        "minutesAfter": minutes_ahead
+    }
+    result = make_one_bus_away_api_call(f"arrivals-and-departures-for-stop/{stop_id}.json", params)
     write_file_path = f"{stop_id}_arrivals_and_departures.json"
     with open(write_file_path,"w") as f:
         json.dump(result,f)
@@ -238,7 +239,7 @@ async def get_next_stop(stop_id) ->set:
 # test bed
 if __name__ == "__main__":
     print("I am in here")
-    asyncio.run(print_hello("This is a test"))
-    asyncio.run(get_current_time())
+    # asyncio.run(print_hello("This is a test"))
+    # asyncio.run(get_current_time())
     asyncio.run(get_next_stop("1_75403"))
     mcp.run()
